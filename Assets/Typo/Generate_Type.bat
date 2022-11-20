2015ff5c8625711cc303882435de0dd8f39a2e96
FuturiumLab <schmidt@kopffarben.de>
2022-11-20 14:41:24 +0100

    initial commit

---
+++ Assets/Typo/Generate_Type.bat
@@ -0,0 +1,4 @@
+start msdf-bmfont -o Hack-Bold.png          -s 300 -r 4 -p 1 -t msdf -m "2048,2048" -i charset.asci.german.txt .\Hack-Bold.ttf
+start msdf-bmfont -o Hack-BoldItalic.png    -s 300 -r 4 -p 1 -t msdf -m "2048,2048" -i charset.asci.german.txt .\Hack-BoldItalic.ttf
+start msdf-bmfont -o Hack-Italic.png        -s 300 -r 4 -p 1 -t msdf -m "2048,2048" -i charset.asci.german.txt .\Hack-Italic.ttf
+start msdf-bmfont -o Hack-Regular.png       -s 300 -r 4 -p 1 -t msdf -m "2048,2048" -i charset.asci.german.txt .\Hack-Regular.ttf
---
+++ src/BMFont/CharPlacement.cs
@@ -0,0 +1,27 @@
+﻿
+namespace BMFont
+{
+    using Stride.Core.Mathematics; 
+
+    public struct CharPlacement
+    {
+        public Int2 SrcPos;
+        public Int2 SrcSize;
+        public Int2 DesPos;
+        public Int2 DesSize;
+        public CharPlacement(Int2 SrcPos, Int2 SrcSize, Int2 DesPos, Int2 DesSize)
+        {
+            this.SrcPos = SrcPos;
+            this.SrcSize = SrcSize;
+            this.DesPos = DesPos;
+            this.DesSize = DesSize;
+        }
+    }
+
+    public static class Chars
+    {
+        public const char LineBrake = '\n';
+        public const char Return = '\r';
+        /* a few more... */
+    }
+}
